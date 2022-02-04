5f0515fd9f8bacf0fe715b96d69e1aa6a315107c
Aditya <aditya@hashtrust.in>
2022-02-04 13:26:32 +0530

    Added Date  field to About Me

--- MyScript.js
+++ MyScript.js
@@ -1,9 +1,22 @@
-function heading_change(){
+function heading_change() {
+    
     document.getElementById("heading-tag").innerHTML =
-        "A Great One I must add 😂 "
+        "A Great One I must add 😂 ";
 }
 
-function normal_heading(){
+function normal_heading() {
+    
     document.getElementById("heading-tag").innerHTML =
-        "Hello I am Aditya Tak and I am a Software Developer!"
+        "Hello I am Aditya Tak and I am a Software Developer!";
+}
+
+function show_date() {
+    
+    document.getElementById("date").innerHTML = Date();
+}
+
+function normal_option() {
+    
+    document.getElementById("date").innerHTML = 
+        "Current Date & Time";
 }
