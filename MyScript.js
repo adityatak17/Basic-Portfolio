9eaf3b43d42c18bd0456602e90548fd2b966103c
Aditya <aditya@hashtrust.in>
2022-02-04 12:46:51 +0530

    Added Simple JS and made Projects page a little better

---
+++ MyScript.js
@@ -0,0 +1,9 @@
+function heading_change(){
+    document.getElementById("heading-tag").innerHTML =
+        "A Great One I must add 😂 "
+}
+
+function normal_heading(){
+    document.getElementById("heading-tag").innerHTML =
+        "Hello I am Aditya Tak and I am a Software Developer!"
+}
